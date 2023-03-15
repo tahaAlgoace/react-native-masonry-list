85655af471952a1a84fe025aa73c481de2dfe33b
tahaAlgoace <121221340+tahaAlgoace@users.noreply.github.com>
2023-03-15 15:51:40 +0500

    Delete .eslintrc.js

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@dooboo/eslint-config-react-native',
-  rules: {
-    'eslint-comments/no-unlimited-disable': 0,
-    'eslint-comments/no-unused-disable': 0,
-  },
-};
