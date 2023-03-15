ddc4ec1384a51c8bd2bb3c48a90fac64964f383c
tahaAlgoace <muhammad.taha@algoace.com>
2023-03-15 15:29:40 +0500

    done-with-package

--- MasonryExample/test/setupTest.ts
+++
@@ -1,9 +0,0 @@
-import {cleanup} from '@testing-library/react-native';
-
-// Cleanup after each case.
-afterEach(cleanup);
-
-process.on('unhandledRejection', (err) => {
-  // eslint-disable-next-line jest/no-jasmine-globals
-  fail(err);
-});
